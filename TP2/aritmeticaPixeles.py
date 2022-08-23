a7682945cdef3ae7ab404f8a5112fcc47d87e288
CandelaAlbarracin <candelazalbarracin@gmail.com>
2022-08-23 18:50:39 -0300

    Creada la cuasi suma promedio en RGB

--- TP2/aritmeticaPixeles.py
+++ TP2/aritmeticaPixeles.py
@@ -17,3 +17,19 @@ def cuasi_suma_RGB(img1,img2,norm=False):
     else:
         img=np.minimum(img,255)
         return img.astype(np.uint8)
+
+def cuasi_suma_RGB_prom(img1,img2,norm=False):
+    '''
+    img1:matriz nxn
+    img2:matriz nxn
+    norm: indica si los valores de las matrices estan normalizados o no. Por defecto tiene el valor falso
+    Esta función realiza la cuasi-suma en RGB promedio de dos imagenes
+    '''
+    img=np.ndarray(img1.shape)
+    img[:,:,0]=(img1[:,:,0]+img2[:,:,0])/2
+    img[:,:,1]=(img1[:,:,1]+img2[:,:,1])/2
+    img[:,:,2]=(img1[:,:,2]+img2[:,:,2])/2
+    if norm:
+        return img
+    else:
+        return img.astype(np.uint8)
