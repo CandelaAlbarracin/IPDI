93ceed8d00ebe8c8db0f26a6b13a86906d753152
CandelaAlbarracin <candelazalbarracin@gmail.com>
2022-08-23 19:48:50 -0300

    Creada las funciones que realizan las cuasi sumas en YIQ

--- TP2/aritmeticaPixeles.py
+++ TP2/aritmeticaPixeles.py
@@ -33,3 +33,28 @@ def cuasi_suma_RGB_prom(img1,img2,norm=False):
         return img
     else:
         return img.astype(np.uint8)
+
+def cuasi_suma_YIQ(img1,img2):
+    '''
+    img1:matriz nxn
+    img2:matriz nxn
+    Esta función realiza la cuasi-suma en YIQ clampeada de dos imagenes
+    '''
+    img=np.ndarray(img1.shape)
+    img[:,:,0]=img1[:,:,0]+img2[:,:,0]
+    img[:,:,0]=np.minimum(img[:,:,0],1)
+    img[:,:,1]=(img1[:,:,0]*img1[:,:,1]+img2[:,:,0]*img2[:,:,1])/(img1[:,:,0]+img2[:,:,0])
+    img[:,:,2]=(img1[:,:,0]*img1[:,:,2]+img2[:,:,0]*img2[:,:,2])/(img1[:,:,0]+img2[:,:,0])
+    return img
+
+def cuasi_suma_YIQ_prom(img1,img2):
+    '''
+    img1:matriz nxn
+    img2:matriz nxn
+    Esta función realiza la cuasi-suma en YIQ promedio de dos imagenes
+    '''
+    img=np.ndarray(img1.shape)
+    img[:,:,0]=(img1[:,:,0]+img2[:,:,0])/2
+    img[:,:,1]=(img1[:,:,0]*img1[:,:,1]+img2[:,:,0]*img2[:,:,1])/(img1[:,:,0]+img2[:,:,0])
+    img[:,:,2]=(img1[:,:,0]*img1[:,:,2]+img2[:,:,0]*img2[:,:,2])/(img1[:,:,0]+img2[:,:,0])
+    return img
