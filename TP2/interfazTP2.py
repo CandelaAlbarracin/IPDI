452e1158fb57efde05ee415dfd25ada69bdd75db
CandelaAlbarracin <candelazalbarracin@gmail.com>
2022-08-28 21:21:19 -0300

    Mejoras en la interfaz

--- TP2/interfazTP2.py
+++ TP2/interfazTP2.py
@@ -13,14 +13,18 @@ class ventana(QtWidgets.QMainWindow):
         super(ventana, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.cb_formato.addItem("Valor Absoluto")
+        self.ui.cb_op.setEnabled(False)
+        self.ui.cb_formato.setEnabled(False)
+        # self.ui.cb_formato.addItem("Valor Absoluto")
         self.ui.cb_op.addItem("Producto")
         self.ui.cb_op.addItem("Cociente")
+        self.ui.cb_formato.clear()
         self.ui.btn_salir.clicked.connect(self.salir)
         self.ui.btn_img_1.clicked.connect(self.subir_img_1)
         self.ui.btn_img_2.clicked.connect(self.subir_img_2)
-        self.ui.cb_op.currentIndexChanged.connect(self.operar)
+        self.ui.cb_op.currentIndexChanged.connect(self.cargar)
         self.ui.cb_formato.currentIndexChanged.connect(self.operar)
+        self.ui.btn_img_out.clicked.connect(self.guardar)
 
     def salir(self):
         self.close()
@@ -42,6 +46,10 @@ class ventana(QtWidgets.QMainWindow):
                     QtWidgets.QMessageBox.about(self,"Advertencia", "La imagen subida contiene canal alfa (Imagen RGBA). Se ha quitado este canal de la visualización de la imagen original (se muestra la imagen en RGB)")
             else:
                 QtWidgets.QMessageBox.about(self,"Advertencia","La imagen subida no es RGB o RGBA. Intente cambiar la imagen.")
+        else:
+            self.ui.lbl_img_1.setText("Seleccione una imagen")
+            self.ui.lbl_img_out.setText("Seleccione dos imagenes para operar")
+        self.habilitar_btns()
 
     def subir_img_2(self):
         global dir2
@@ -60,6 +68,24 @@ class ventana(QtWidgets.QMainWindow):
                     QtWidgets.QMessageBox.about(self,"Advertencia", "La imagen subida contiene canal alfa (Imagen RGBA). Se ha quitado este canal de la visualización de la imagen original (se muestra la imagen en RGB)")
             else:
                 QtWidgets.QMessageBox.about(self,"Advertencia","La imagen subida no es RGB o RGBA. Intente cambiar la imagen.")
+        else:
+            self.ui.lbl_img_2.setText("Seleccione una imagen")
+            self.ui.lbl_img_out.setText("Seleccione dos imagenes para operar")
+        self.habilitar_btns()
+    
+    def habilitar_btns(self):
+        if self.ui.lbl_img_2.pixmap() and self.ui.lbl_img_1.pixmap():
+            self.ui.cb_op.setEnabled(True)
+            self.ui.cb_formato.setEnabled(True)
+
+    def cargar(self):
+        opciones={"Cuasi-Suma":["RGB clamping","RGB promedio","YIQ clamping","YIQ promedio"],"Cuasi-Resta":["RGB clamping","RGB promedio","YIQ clamping","YIQ promedio","Valor Absoluto"],"if-ligther":["RGB","YIQ"],"if-darker":["RGB","YIQ"],"Producto":[],"Cociente":[]}
+        op=self.ui.cb_op.currentText()
+        if op in opciones:
+            self.ui.cb_formato.clear()
+            self.ui.cb_formato.addItem("")
+            self.ui.cb_formato.addItems(opciones[op])
+        self.operar()
 
     def operar(self):
         op=self.ui.cb_op.currentText()
@@ -122,6 +148,7 @@ class ventana(QtWidgets.QMainWindow):
 
 
     def mostrar_resultado(self,img):
+        global im
         rgb=img*255
         rgb=np.minimum(rgb,255)
         rgb=np.maximum(rgb,0)
@@ -129,6 +156,15 @@ class ventana(QtWidgets.QMainWindow):
         im = QtGui.QImage(rgb.astype(np.uint8), w, h, 3*w,QtGui.QImage.Format_RGB888)
         pix = QtGui.QPixmap.fromImage(im)
         self.ui.lbl_img_out.setPixmap(pix)
+    
+    def guardar(self):
+        if self.ui.lbl_img_out.pixmap():
+            path, _ = QtWidgets.QFileDialog.getSaveFileName(self, "Guardar Imagen", "", "PNG(*.png)") 
+            if path != "": 
+                im.save(path)
+        else:
+            QtWidgets.QMessageBox.about(self,"Advertencia","Realice alguna operación con las imagenes para poder guardar su resultado")
+
 
 app = QtWidgets.QApplication([])
 application = ventana()
